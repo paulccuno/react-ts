23895997a06fd459f40f00ca095f5f241d1c6921
paulccuno <pallccuno@gmail.com>
2023-01-21 12:36:43 -0500

    initial config react-router-dom v5

---
+++ src/routes/NavigationV5.tsx
@@ -0,0 +1,48 @@
+import { RoutereBrowser as Router, Switch, Route, NavLink } from 'react-router-dom'
+import logo from '../assets/react.svg'
+
+/**
+ * Need to install react-router-dom V5, using: npm install react-router-dom@5
+ * Also need to install a dev dependecie for use typescript: npm install -D @types/react-router-dom
+ */
+
+export const NavigationV5 = () => {
+	return (
+		<Router>
+			<div className="main-layout">
+				<nav>
+					<img src={logo} alt="React Logo" />
+					<ul>
+						<li>
+							<NavLink to="/" activeClassName="nav-active" exact>
+								Home
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to="/about" activeClassName="nav-active" exact>
+								About
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to="/users" activeClassName="nav-active" exact>
+								Users
+							</NavLink>
+						</li>
+					</ul>
+				</nav>
+
+				<Switch>
+					<Route path="/about">
+						<h1>About</h1>
+					</Route>
+					<Route path="/users">
+						<h1>users</h1>
+					</Route>
+					<Route path="/">
+						<h1>Home</h1>
+					</Route>
+				</Switch>
+			</div>
+		</Router>
+	)
+}
