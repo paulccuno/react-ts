a6d610d077fb41eaf8b11cb460deb44f2c83bd90
paulccuno <pallccuno@gmail.com>
2023-01-23 11:45:11 -0500

    initial routing configuration

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Navigate, NavLink, Route, Routes } from 'react-router-dom'
+import { routes } from './routes'
 
 import logo from '../assets/react.svg'
 
@@ -10,30 +11,22 @@ export const Navigation = () => {
 					<img src={logo} alt="Vite Logo" />
 
 					<ul>
-						<li>
-							<NavLink to="/home" className={({ isActive }) => (isActive ? 'nav-active' : '')}>
-								Home
-							</NavLink>
-						</li>
-						<li>
-							<NavLink to="/about" className={({ isActive }) => (isActive ? 'nav-active' : '')}>
-								About
-							</NavLink>
-						</li>
-						<li>
-							<NavLink to="/users" className={({ isActive }) => (isActive ? 'nav-active' : '')}>
-								Users
-							</NavLink>
-						</li>
+						{routes.map(({ to, name }) => (
+							<li key={to}>
+								<NavLink to={to} className={({ isActive }) => (isActive ? 'nav-active' : '')}>
+									{name}
+								</NavLink>
+							</li>
+						))}
 					</ul>
 				</nav>
 
 				<Routes>
-					<Route path="about" element={<h1>About Page</h1>} />
-					<Route path="users" element={<h1>Users Page</h1>} />
-					<Route path="/home" element={<h1>Home Page</h1>} />
+					{routes.map(({ path, Component }) => (
+						<Route key={path} path={path} element={<Component />} />
+					))}
 
-					<Route path="/*" element={<Navigate to="/home" replace />} />
+					<Route path="/*" element={<Navigate to={routes[0].to} replace />} />
 				</Routes>
 			</div>
 		</BrowserRouter>
