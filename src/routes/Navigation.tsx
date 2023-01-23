0db22ea4e5b7ab3c4df763d351d4246b9bf1e734
paulccuno <pallccuno@gmail.com>
2023-01-23 11:45:41 -0500

    lazy config to routes

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -2,33 +2,36 @@ import { BrowserRouter, Navigate, NavLink, Route, Routes } from 'react-router-do
 import { routes } from './routes'
 
 import logo from '../assets/react.svg'
+import { Suspense } from 'react'
 
 export const Navigation = () => {
 	return (
-		<BrowserRouter>
-			<div className="main-layout">
-				<nav>
-					<img src={logo} alt="Vite Logo" />
+		<Suspense fallback={<span>Loading...</span>}>
+			<BrowserRouter>
+				<div className="main-layout">
+					<nav>
+						<img src={logo} alt="Vite Logo" />
 
-					<ul>
-						{routes.map(({ to, name }) => (
-							<li key={to}>
-								<NavLink to={to} className={({ isActive }) => (isActive ? 'nav-active' : '')}>
-									{name}
-								</NavLink>
-							</li>
+						<ul>
+							{routes.map(({ to, name }) => (
+								<li key={to}>
+									<NavLink to={to} className={({ isActive }) => (isActive ? 'nav-active' : '')}>
+										{name}
+									</NavLink>
+								</li>
+							))}
+						</ul>
+					</nav>
+
+					<Routes>
+						{routes.map(({ path, Component }) => (
+							<Route key={path} path={path} element={<Component />} />
 						))}
-					</ul>
-				</nav>
 
-				<Routes>
-					{routes.map(({ path, Component }) => (
-						<Route key={path} path={path} element={<Component />} />
-					))}
-
-					<Route path="/*" element={<Navigate to={routes[0].to} replace />} />
-				</Routes>
-			</div>
-		</BrowserRouter>
+						<Route path="/*" element={<Navigate to={routes[0].to} replace />} />
+					</Routes>
+				</div>
+			</BrowserRouter>
+		</Suspense>
 	)
 }
