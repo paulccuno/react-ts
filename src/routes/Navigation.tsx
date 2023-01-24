5737632b08e6951b69f0f8e88a76866825574068
paulccuno <pallccuno@gmail.com>
2023-01-23 23:36:39 -0500

    nested lazyload configuration

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -18,7 +18,7 @@ export const Navigation = () => {
 						<ul>
 							{routes.map(({ path, name }) => (
 								<li key={path}>
-									<NavLink to={path} activeClassName="nav-active" exact>
+									<NavLink to={path} activeClassName="nav-active">
 										{name}
 									</NavLink>
 								</li>
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,5 +1,5 @@
 import { lazy, LazyExoticComponent } from 'react'
-// import { LazyPage1, LazyPage2, LazyPage3 } from '../01-lazyload/pages'
+import { NoLazy } from '../01-lazyload/pages'
 
 type JSXComponent = () => JSX.Element
 
@@ -10,35 +10,19 @@ interface Route {
 	children?: Route[]
 }
 
-const LazyPage1 = lazy(
-	() => import(/* webpackChunkName: "LazyPage1" */ '../01-lazyload/pages/LazyPage1')
-)
-const LazyPage2 = lazy(
-	() => import(/* webpackChunkName: "LazyPage2" */ '../01-lazyload/pages/LazyPage2')
-)
-const LazyPage3 = lazy(
-	() => import(/* webpackChunkName: "LazyPage3" */ '../01-lazyload/pages/LazyPage3')
+const LazyLayout = lazy(
+	() => import(/* webpackChunkName: "LazyLayout" */ '../01-lazyload/layout/LazyLayout')
 )
 
 export const routes: Route[] = [
 	{
-		path: '/lazy1',
-		component: LazyPage1,
-		name: 'LazyPage-1',
+		path: '/lazyload',
+		component: LazyLayout,
+		name: 'LazyLoading Nested',
 	},
 	{
-		path: '/lazy2',
-		component: LazyPage2,
-		name: 'LazyPage-2',
-	},
-	{
-		path: '/lazy3',
-		component: LazyPage3,
-		name: 'LazyPage-3',
-	},
-	{
-		path: '/lazy4',
-		component: LazyPage3,
-		name: 'LazyPage-4',
+		path: '/nolazy',
+		component: NoLazy,
+		name: 'No LazyLoading',
 	},
 ]
