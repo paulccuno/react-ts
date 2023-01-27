4751321c919cc1ee4e0f7e2b6b486b5233005707
paulccuno <pallccuno@gmail.com>
2023-01-27 13:33:07 -0500

    Control Props alt

--- src/02-component-patterns/hooks/useShoppingCart.ts
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -6,30 +6,15 @@ export const useShoppingCart = () => {
 
 	const onProductCountChange = ({ count, product }: { count: number; product: Product }) => {
 		setShoppingCart(prevShoppingCart => {
-			const productInCart: ProductInCart = prevShoppingCart[product.id] || { ...product, count: 0 }
-
-			if (Math.max(productInCart.count + count, 0) > 0) {
-				productInCart.count += count
-				return {
-					...prevShoppingCart,
-					[product.id]: productInCart,
-				}
+			if (count === 0) {
+				const { [product.id]: toDelete, ...rest } = prevShoppingCart
+				return rest
 			}
 
-			const { [product.id]: toDelete, ...rest } = prevShoppingCart
-			return rest
-
-			// * Execution of the change event without having control of the value returned by the ProductCard component
-			// * Ejecucion del evento change sin tener control de value retornado por el componente ProductCard
-			// if (count === 0) {
-			// 	const { [product.id]: toDelete, ...rest } = prevShoppingCart
-			// 	return rest
-			// }
-
-			// return {
-			// 	...prevShoppingCart,
-			// 	[product.id]: { ...product, count },
-			// }
+			return {
+				...prevShoppingCart,
+				[product.id]: { ...product, count },
+			}
 		})
 	}
 
